71fe21a449ba64582fc866da904b44eece80e561
Michal Stanke <michal@stanke.cz>
2017-01-16 00:00:06 +0100

    Import Spring Initializr demo project (fix #1)

---
+++ src/main/java/cz/stanke/java/spring/boot/init/ApplicationInit.java
@@ -0,0 +1,12 @@
+package cz.stanke.java.spring.boot.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInit {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationInit.class, args);
+    }
+}
